b6a796ab6b7809a23a3432204d1ca4dc86730b45
Ajay Bandagonda <bandagonda.ajay8@gmail.com>
2021-05-16 18:46:07 -0400

    Added timer class

--- pomodoro.js
+++ pomodoro.js
@@ -2,20 +2,75 @@ let secondsLabel = document.querySelector("#seconds")
 let minutesLabel = document.querySelector("#minutes")
 let stopBtn = document.querySelector("#stopTimer")
 let studyMinutes = document.querySelector("#studySelect")
+let sessionType = document.querySelector("#sessionType")
 let form = document.querySelector("form")
 
-let secondsCounter, minutesCounter;
+let secondsCounter;
 let sec = 0
 let mins = 30
 
+class Timer {
+    constructor(minutes, seconds, minutesLabel, secondsLabel, sessionLabel, sessionType = 'study') {
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.secondsLabel = secondsLabel
+        this.minutesLabel = minutesLabel
+        this.sessionType = sessionType
+        this.sessionLabel = sessionLabel
+        this.isFinished = false
+    }
+    start() {
+        this.timer = setInterval(() => {
+            if (this.minutes <= 0 && this.seconds <= 0) {
+                this.stop()
+                this.isFinished = true
+            }
+            else if (this.seconds == 0) {
+                this.seconds = 59
+                if (this.minutes > 0) {
+                    this.minutes--;
+                }
+            }
+            if (this.minutes < 10) {
+                this.minutesLabel.innerHTML = `0${this.minutes}`
+            }
+            else {
+                this.minutesLabel.innerHTML = `${this.minutes}`
+            }
+            if (this.seconds < 10) {
+                this.secondsLabel.innerHTML = `:0${this.seconds--}`
+            } 
+            else {
+                this.secondsLabel.innerHTML = `:${this.seconds--}`
+            }
+        }, 1000);
+        this.setMessage()
+    }
+    stop(){
+        clearInterval(this.timer)
+    }
+    setMessage(){
+        if (this.sessionType === 'break') {
+            this.sessionLabel.innerText = "Enjoy your short break!"
+        }
+        else {
+            this.sessionLabel.innerText = "Study Session in progress! Focus!!"
+        }
+    }
+}
+
+var timer = new Timer(0, 5, minutesLabel, secondsLabel, sessionType)
+
 stopBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    clearInterval(secondsCounter)
+    timer.stop()
 })
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    timer(mins, sec)
+    //timer(0, 5)
+    
+    timer.start()
 })
 
 studyMinutes.addEventListener("change", (e) => {
@@ -23,16 +78,26 @@ studyMinutes.addEventListener("change", (e) => {
     minutesLabel.innerHTML = `${studyMinutes.value}`
 })
 
-function timer(minutes, seconds) {
+/*const timer = (minutes, seconds=0) => {
     secondsCounter = setInterval(myClock, 1000);
+    let isBreak = false;
 
     function myClock() {
         if (minutes <= 0 && seconds <= 0) {
-            clearInterval(secondsCounter);
+            if (isBreak) {
+                clearInterval(secondsCounter);
+            }
+            else{
+                minutes = 1
+                seconds = 0
+                isBreak = true
+            }
         }
-        else if (seconds == 0) {
+        if (seconds == 0) {
             seconds = 59
-            minutes--;
+            if (minutes > 0) {
+                minutes--;
+            }
         }
         if (minutes < 10) {
             minutesLabel.innerHTML = `0${minutes}`
@@ -46,5 +111,11 @@ function timer(minutes, seconds) {
         else {
             secondsLabel.innerHTML = `:${seconds--}`
         }
+        if (isBreak) {
+            sessionType.innerText = "Enjoy your short break!"
+        } 
+        else {
+            sessionType.innerText = "Study Session in progress! Focus!!"
+        }
     }
-}
+}*/
